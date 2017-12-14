57bdc833a31de36c8361f367be78ab192a792a78
EasternCatNZL <brianpwwong@gmail.com>
2017-12-14 17:08:52 +1300

    Out of screen tracker done, needs icon and other adjustments

---
+++ Hogei/Assets/Scripts/Enemy/RoomShrimpAddon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomShrimpAddon : MonoBehaviour {
+
+    public GameObject myIndicator;
+
+	// Use this for initialization
+	void Start () {
+        GameObject indicatorClone = Instantiate(myIndicator, transform.position, transform.rotation);
+        indicatorClone.GetComponent<EnemyOutOfScreenTracker>().SetTarget(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Hogei/Assets/Scripts/UI/EnemyOutOfScreenTracker.cs
+++ Hogei/Assets/Scripts/UI/EnemyOutOfScreenTracker.cs
@@ -10,9 +10,10 @@ public class EnemyOutOfScreenTracker : MonoBehaviour {
 
     [Header("Image")]
     [Tooltip("Indicator object")]
-    public GameObject indicatorObject;
+    //public GameObject indicatorObject;
+    public GameObject indicatorImageObject;
     [Tooltip("Indicator image")]
-    public Sprite indicator;
+    public SpriteRenderer indicator;
     [Tooltip("Offset distance for image along edge of screen")]
     public float imageOffsetDistance = 3.0f;
     [Tooltip("Y plane for indicator")]
@@ -24,6 +25,7 @@ public class EnemyOutOfScreenTracker : MonoBehaviour {
 
     //player ref
     private GameObject player;
+    private GameObject target;
 
     //control vars
     private bool inView = false;
@@ -31,10 +33,17 @@ public class EnemyOutOfScreenTracker : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag(playerTag);
+        mainCamera = Camera.main;
+
+        //debug
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //GameObject sphere2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, 10));
+        //sphere2.transform.position = new Vector3(screenBoundsMax.x, 1.5F, screenBoundsMax.y);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         CheckInCameraView();
         TrackOffScreenTarget();
 	}
@@ -43,13 +52,15 @@ public class EnemyOutOfScreenTracker : MonoBehaviour {
     private void CheckInCameraView()
     {
         //if in camera's viewport
-        if (GeometryUtility.TestPlanesAABB(GeometryUtility.CalculateFrustumPlanes(mainCamera), GetComponent<Collider>().bounds))
+        if (GeometryUtility.TestPlanesAABB(GeometryUtility.CalculateFrustumPlanes(mainCamera), target.GetComponent<Collider>().bounds))
         {
             inView = true;
+            indicator.color = new Color(255.0f, 255.0f, 255.0f, 0.0f);
         }
         else
         {
             inView = false;
+            indicator.color = new Color(255.0f, 255.0f, 255.0f, 255.0f);
         }
     }
 
@@ -59,27 +70,55 @@ public class EnemyOutOfScreenTracker : MonoBehaviour {
         //if not in view
         if (!inView)
         {
+
             //get direction to target from player
-            Vector3 direction = transform.position - player.transform.position;
+            Vector3 direction = target.transform.position - player.transform.position;
 
             //get the current edge of screen x and z values
-            Vector2 screenMin = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane));
-            Vector2 screenMax = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.nearClipPlane));
+            Vector2 screenMin = new Vector2(0, 0);
+            Vector2 screenMax = new Vector2(Screen.width, Screen.height);
+
+            print("ScreenMin: " + screenMin);
+            print("ScreenMax: " + screenMax);
+
+            //print("Player world pos: " + player.transform.position);
+            //print("Target world pos: " + target.transform.position);
+
+            //get the screen pos of player and target
+            Vector2 playerScreenPos = mainCamera.WorldToScreenPoint(player.transform.position);
+            Vector2 targetScreenPos = mainCamera.WorldToScreenPoint(target.transform.position);
+
+            //print("Player screen pos: " + playerScreenPos);
+            //print("Target screen pos: " + targetScreenPos);
+
+            //convert the screen pos to gui pos
+            Vector2 playerGUIPos = GUIUtility.ScreenToGUIPoint(playerScreenPos);
+            Vector2 targetGUIPos = GUIUtility.ScreenToGUIPoint(targetScreenPos);
 
             //set the offset of edges of screen
             Vector2 screenBoundsMin = new Vector2(screenMin.x + imageOffsetDistance, screenMin.y + imageOffsetDistance);
             Vector2 screenBoundsMax = new Vector2(screenMax.x - imageOffsetDistance, screenMax.y - imageOffsetDistance);
 
-            //position the indicator in the world where the target is
-            indicatorObject.transform.position = transform.position;
-            //clamp the x and z pos by screen bounds
-            float newX = Mathf.Clamp(indicatorObject.transform.position.x, screenBoundsMin.x, screenBoundsMax.x);
-            float newZ = Mathf.Clamp(indicatorObject.transform.position.z, screenBoundsMin.y, screenBoundsMax.y);
-            //set new position
-            indicatorObject.transform.position = new Vector3(newX, yPlane, newZ);
+            ////position the indicator in the world where the target is
+            //transform.position = target.transform.position;
 
-            //rotate to face the target
-            indicatorObject.transform.rotation = Quaternion.LookRotation(direction);
+            //clamp the x and z pos by screen bounds
+            float newX = Mathf.Clamp(targetScreenPos.x, screenBoundsMin.x, screenBoundsMax.x);
+            float newZ = Mathf.Clamp(targetScreenPos.y, screenBoundsMin.y, screenBoundsMax.y);
+
+            print(newX);
+            print(newZ);
+
+            //set new position
+            //Vector2 inScreenPos = new Vector2(newX, newZ);
+            Vector3 inWorldPos = mainCamera.ScreenToWorldPoint(new Vector3(newX, newZ, yPlane));
+            transform.position = inWorldPos;
+
         }
+    }
+
+    public void SetTarget(GameObject targetObject)
+    {
+        target = targetObject;
     }
 }
