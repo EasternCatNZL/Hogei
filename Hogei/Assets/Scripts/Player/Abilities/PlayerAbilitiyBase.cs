ae7225843cca714be310a0da7edab814af3b1c25
EasternCatNZL <brianpwwong@gmail.com>
2017-12-23 16:38:47 +1300

    Newer unity version

---
+++ Hogei/Assets/Scripts/Player/Abilities/PlayerAbilitiyBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilitiyBase : MonoBehaviour {
+
+    [Header("Inputs")]
+    [Tooltip("Slow ability key")]
+    public KeyCode abilityOneKey = KeyCode.F;
+    [Tooltip("Shield ablity key")]
+    public KeyCode abilityTwoKey = KeyCode.G;
+
+    //[Header("Ability Scripts")]
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
