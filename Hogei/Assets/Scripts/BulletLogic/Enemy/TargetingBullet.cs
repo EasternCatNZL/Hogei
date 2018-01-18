835137bb55463564b2431f1c740624bc6664ea8e
EasternCatNZL <brianpwwong@gmail.com>
2018-01-18 16:45:23 +1300

    New enemy bullet type and pattern

---
+++ Hogei/Assets/Scripts/BulletLogic/Enemy/TargetingBullet.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TargetingBullet : MonoBehaviour {
+
+    [Header("Damage")]
+    [Tooltip("Damage dealt by bullet")]
+    public float bulletDamage = 1.0f;
+
+    [Header("Speed")]
+    [Tooltip("Travel speed of bullet")]
+    public float travelSpeed = 2.0f;
+
+    //set up vars
+    [HideInInspector]
+    public Vector3 setupDestination = new Vector3(0, 0, 0);
+    [HideInInspector]
+    public float setupDestinationDistance = 0.0f;
+    [HideInInspector]
+    public float setupTime = 0.0f;
+    [HideInInspector]
+    public float startDelay = 0.0f;
+    [HideInInspector]
+    public string targetTag = "Player";
+
+    //control vars
+    private float startTime = 0.0f;
+    public bool isStarting = false;
+    private bool isActive = false;
+
+    private Rigidbody myRigid;
+
+    // Use this for initialization
+    void Start () {
+        myRigid = GetComponent<Rigidbody>();
+        startTime = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isStarting)
+        {
+            SetUp();
+        }
+    }
+
+    //sets up vars for bullet behaviour
+    public void SetupVars(float dist, float setTime, float delay, float speed, string tag)
+    {
+        setupDestinationDistance = dist;
+        setupTime = setTime;
+        startDelay = delay;
+        travelSpeed = speed;
+        isStarting = true;
+        targetTag = tag;
+
+        print("Variables set");
+    }
+
+    //func called when setup ready
+    private void SetUp()
+    {
+        isStarting = false;
+        setupDestination = transform.position + (transform.forward * setupDestinationDistance);
+        transform.DOMove(setupDestination, setupTime, false);
+        //play audio
+        //bulletFireSound.Play();
+        //transform.DOMove(new Vector3(2, 1, 3), 2, false);
+        StartCoroutine(BeginMove());
+
+        print("Set up");
+    }
+
+    private IEnumerator BeginMove()
+    {
+        print("Beggining move");
+
+        //waits for setup to finish
+        yield return new WaitForSecondsRealtime(startDelay);
+        //gets a new rotation
+        Quaternion newRotation = new Quaternion();
+
+        //set facing target
+        transform.LookAt(GameObject.FindGameObjectWithTag(targetTag).transform.position);
+
+        //start moving
+        myRigid.velocity = transform.forward * travelSpeed;
+        //play audio
+        //bulletChangeDirectionSound.Play();
+    }
+}
---
+++ Hogei/Assets/Scripts/PatternScripts/Aimed/CircleReleaseAimedShot.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleReleaseAimedShot : MonoBehaviour {
+
+    [Header("Timing vars")]
+    [Tooltip("Time Between Sprays")]
+    public float timeBetweenSprays = 3.0f;
+
+    [Header("Bullet Vars")]
+    [Tooltip("Bullet object")]
+    public GameObject bulletObject;
+    [Tooltip("Number of bullets per spray")]
+    public int numBulletsPerSpray = 12;
+
+    [Header("Bullet set up vars")]
+    [Tooltip("Bullet base setup distance")]
+    public float bulletBaseSetupDistance = 2.5f;
+    [Tooltip("Bullet setup move time")]
+    public float bulletSetupTime = 0.5f;
+    [Tooltip("Bullet active move start time")]
+    public float bulletStartMoveTimeDelay = 0.2f;
+    [Tooltip("Bullet travel speed")]
+    public float bulletTravelSpeed = 5.0f;
+
+    [Header("Tags")]
+    [Tooltip("Target tag")]
+    public string targetTag = "Player";
+
+    //script refs
+    private EnemyState enemyState;
+
+    //control vars
+    private float timeLastSprayFired = 0.0f; //the time last spray began
+    private float currentAngleTotal = 0.0f; //the current angle the bullet is angled at in regards to owner
+    private float angleChangePerShot = 0.0f; //the angle change between each shot
+
+    // Use this for initialization
+    void Start () {
+        enemyState = GetComponent<EnemyState>();
+        //get angle change value
+        angleChangePerShot = 360 / numBulletsPerSpray;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (enemyState.GetIsActive())
+        {
+            if (Time.time > timeLastSprayFired + timeBetweenSprays)
+            {
+                BulletSpray();
+            }
+        }
+    }
+
+    //bullet spray function
+    private void BulletSpray()
+    {
+        //set time of last spray to now
+        timeLastSprayFired = Time.time;
+
+        //float to track angle change
+        float angleChange = 0;
+        //until angle change >= 360
+        while(angleChange < 360)
+        {
+            //get the current angle as quaternion
+            Quaternion currentRotation = new Quaternion();
+            currentRotation.eulerAngles = new Vector3(0.0f, angleChange, 0.0f);
+
+            //create a bullet
+            GameObject bulletClone = Instantiate(bulletObject, transform.position, transform.rotation);
+
+            //set rotation to current rotation
+            bulletClone.transform.rotation = currentRotation;
+
+            //setup bullet variables
+            bulletClone.GetComponent<TargetingBullet>().SetupVars(bulletBaseSetupDistance, bulletSetupTime, bulletSetupTime + bulletStartMoveTimeDelay, bulletTravelSpeed, targetTag);
+
+            //increment angle change
+            angleChange += angleChangePerShot;
+        }
+    }
+}
