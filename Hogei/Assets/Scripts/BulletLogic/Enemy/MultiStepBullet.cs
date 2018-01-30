efa1c3a5fcc8a74c0e8190203ef7a90668f922bd
EasternCatNZL <brianpwwong@gmail.com>
2018-01-30 16:48:52 +1300

    More bullet stuff

---
+++ Hogei/Assets/Scripts/BulletLogic/Enemy/MultiStepBullet.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MultiStepBullet : MonoBehaviour {
+
+    [Header("Damage")]
+    [Tooltip("Damage dealt by bullet")]
+    public float bulletDamage = 1.0f;
+
+    [Header("Steps")]
+    public int numSteps = 0;
+
+    //set up vars
+    [HideInInspector]
+    public float travelSpeed = 2.0f;
+    [HideInInspector]
+    public List<float> setupDistances = new List<float>();
+    [HideInInspector]
+    public List<float> angleChanges = new List<float>();
+    [HideInInspector]
+    public float setupTime = 0.0f;
+    [HideInInspector]
+    public float startDelay = 0.0f;
+
+    //control vars
+    private int currentStep = 0;
+    private float startTime = 0.0f;
+    public bool isStarting = false;
+    private bool isMoving = false;
+    private bool stepsFinished = false;
+    private Vector3 currentDestination = Vector3.zero;
+
+    private Rigidbody myRigid;
+
+    // Use this for initialization
+    void Start () {
+        myRigid = GetComponent<Rigidbody>();
+        startTime = Time.time;
+        currentDestination = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (CheckArrived() && !stepsFinished)
+        {
+            Step();
+        }
+    }
+
+    //sets up vars for bullet behaviour
+    public void SetupVars(float setTime, float speed, int steps)
+    {
+        setupTime = setTime;
+        travelSpeed = speed;
+        numSteps = steps;
+        isStarting = true;
+        //Step();
+    }
+
+    //step func
+    public void Step()
+    {
+        //manage rotation
+        if (currentStep > 0 && currentStep <= numSteps)
+        {
+            Quaternion newRotation = new Quaternion();
+            //alters rotation based own rotation + given rotation
+            newRotation.eulerAngles = new Vector3(0.0f, transform.rotation.eulerAngles.y + angleChanges[currentStep - 1], 0.0f);
+            transform.rotation = newRotation;
+        }
+        //manage movement
+        //if finished steps, move straight
+        if (currentStep == numSteps)
+        {
+            myRigid.velocity = transform.forward * travelSpeed;
+            stepsFinished = true;
+        }
+        //move to next step
+        else
+        {
+            //set new destination using setup distance
+            currentDestination = transform.position + (transform.forward * setupDistances[currentStep]);
+            //begin the movement
+            transform.DOMove(currentDestination, setupTime, false);
+            //increment step
+            currentStep++;
+        }
+    }
+
+    private bool CheckArrived()
+    {
+        bool hasArrived = false;
+        if(transform.position == currentDestination)
+        {
+            hasArrived = true;
+        }
+        return hasArrived;
+    }
+}
---
+++ Hogei/Assets/Scripts/PatternScripts/FancyStuffAKAImitated/EoSD/Selaginella9.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selaginella9 : MonoBehaviour {
+
+    [Header("Timing Vars")]
+    [Tooltip("Time Between Shots")]
+    public float timeBetweenShots = 0.2f;
+
+    [Header("Bullet Vars")]
+    [Tooltip("Bullet object")]
+    public GameObject bulletObject;
+
+    [Header("Bullet set up vars")]
+    [Tooltip("Bullet setup distance")]
+    public float[] bulletSetUpDistanceArray = new float[0];
+    [Tooltip("Bullet angle changes")]
+    public float[] bulletAngleChangeArray = new float[0];
+    [Tooltip("Bullet setup move time")]
+    public float bulletSetupTime = 0.5f;
+    [Tooltip("Pattern bullet set speed")]
+    public float bulletTravelSpeed = 3.0f;
+    [Tooltip("The number of steps")]
+    public int steps = 2;
+
+    [Header("Angle Control")]
+    [Tooltip("Angle change per shot")]
+    [Range(0.0f, 360.0f)]
+    public float angleChangePerShot = 4.0f;
+    [Tooltip("Angle change per shot in spray")]
+    [Range(0.0f, 360.0f)]
+    public float angleChangePerSpray = 60.0f;
+
+    //script refs
+    //private BulletBank bank;
+    private EnemyState enemyState;
+
+    //control vars
+    private float timeLastSprayFired = 0.0f; //the time last spray began
+    private float currentAngle = 0.0f; //the current angle the bullet is angled at in regards to owner
+
+    // Use this for initialization
+    void Start () {
+        enemyState = GetComponent<EnemyState>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (enemyState.GetIsActive())
+        {
+            if (Time.time > timeLastSprayFired + timeBetweenShots)
+            {
+                BulletSpray();
+            }
+        }
+    }
+
+    //bullet spray function
+    private void BulletSpray()
+    {
+        //set time of last spray to now
+        timeLastSprayFired = Time.time;
+
+        //reset current angle total between bullet rings
+        float currentAngleTotal = 0.0f;
+        //while current angle total not reached 360, keep spawning bullets
+        while (currentAngleTotal < 360.0f)
+        {
+            //create a shot
+            GameObject bulletClone = Instantiate(bulletObject, transform.position, transform.rotation);
+            //rotate it to face desired rotation
+            Quaternion currentRotation = new Quaternion();
+            currentRotation.eulerAngles = new Vector3(0.0f, currentAngle + currentAngleTotal, 0.0f);
+            bulletClone.transform.rotation = currentRotation;
+            //set up setup distances and angle changes
+            for (int i = 0; i < steps; i++)
+            {
+                bulletClone.GetComponent<MultiStepBullet>().setupDistances.Add(bulletSetUpDistanceArray[i]);
+                bulletClone.GetComponent<MultiStepBullet>().angleChanges.Add(bulletAngleChangeArray[i]);
+            }
+            //set up bullet variables
+            bulletClone.GetComponent<MultiStepBullet>().SetupVars(bulletSetupTime, bulletTravelSpeed, steps);
+
+            //create second shot in reverse direction
+            GameObject bulletClone2 = Instantiate(bulletObject, transform.position, transform.rotation);
+            //rotate it to face desired rotation
+            currentRotation = new Quaternion();
+            currentRotation.eulerAngles = new Vector3(0.0f, -currentAngle + -currentAngleTotal, 0.0f);
+            bulletClone2.transform.rotation = currentRotation;
+            //set up setup distances and angle changes
+            for (int i = 0; i < steps; i++)
+            {
+                bulletClone2.GetComponent<MultiStepBullet>().setupDistances.Add(bulletSetUpDistanceArray[i]);
+                bulletClone2.GetComponent<MultiStepBullet>().angleChanges.Add(-bulletAngleChangeArray[i]);
+            }
+            //set up bullet variables
+            bulletClone2.GetComponent<MultiStepBullet>().SetupVars(bulletSetupTime, bulletTravelSpeed, steps);
+
+            //change angle between bullets in same spray
+            currentAngleTotal += angleChangePerSpray;
+        }
+        //change angle between shots
+        currentAngle += angleChangePerShot;
+    }
+}
