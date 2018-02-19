2daf6bc9bed322f5a156747c5db672ed89ca6c19
Unknown <brianpwwong@gmail.com>
2018-02-19 17:10:35 +1300

    Started on outlaw behaviour

---
+++ Hogei/Assets/Scripts/Enemy/ItsHighNoon/OutlawBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class OutlawBehaviour : MonoBehaviour {
+
+    //set up vars
+    [HideInInspector]
+    public float setupDistance = 0.0f;
+    [HideInInspector]
+    public float setupTime = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //setup vars
+    public void SetupVars(float setupDist, float time)
+    {
+        setupDistance = setupDist;
+        setupTime = time;
+    }
+
+    //move
+    public void MoveToSetupLocation()
+    {
+        //get the location to setup to
+        Vector3 locationToSetUp = transform.position + (transform.forward * setupDistance);
+    }
+}
