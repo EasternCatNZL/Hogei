1bd6221b5144b6261d36e6d9fabf00b1566b4f0b
Unknown <brianpwwong@gmail.com>
2018-02-19 13:34:21 +1300

    Staging to push stuff

---
+++ Hogei/Assets/Prefab/Enemy/ItsHighNoon/OutlawGroupBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlawGroupBehaviour : MonoBehaviour {
+
+    [Header("Setup vars")]
+    [Tooltip("Setup starting distance")]
+    public float startDistance = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
