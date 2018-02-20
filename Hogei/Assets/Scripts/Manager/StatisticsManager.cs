624731e701614c42f1639cf6ea38c7564d9a1e51
Casey <chimpsterchip@gmail.com>
2018-02-20 16:56:29 +1300

    Fixed YAML... YMAL? errors. Started Stats tracking

---
+++ Hogei/Assets/Scripts/Manager/StatisticsManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatisticsManager : MonoBehaviour {
+
+    int EnemyDeathCount = 0;
+    public static StatisticsManager Singleton;
+
+	// Use this for initialization
+	void Start () {
+		if (Singleton == null)
+        {
+            Singleton = this;
+        }
+	}
+
+    void OnEnable()
+    {
+        EntityHealth.OnDeath += IncreaseDeathCount;
+    }
+
+    void OnDisable()
+    {
+        EntityHealth.OnDeath -= IncreaseDeathCount;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void IncreaseDeathCount()
+    {
+        EnemyDeathCount += 1;
+    }
+}
