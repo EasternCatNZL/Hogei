65ad8f46de1f248d75c67e993426a20460e9e002
Brian <brianpwwong@gmail.com>
2018-02-22 12:55:54 +1300

    Auto stash before merge of "master" and "origin/master"
    Started on debug tools, invincibility toggle done

---
+++ Hogei/Assets/Scripts/Utils/DebugTools.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTools : MonoBehaviour {
+
+    [Header("Tags")]
+    [Tooltip("Player tag")]
+    public string playerTag = "Player";
+
+    [Header("Key codes")]
+    [Tooltip("Key to toggle debug tools")]
+    public KeyCode debugToolToggleKey = KeyCode.F1;
+    [Tooltip("Key to toggle player invincibility")]
+    public KeyCode invincibilityToggleKey = KeyCode.F2;
+
+    //Object refs
+    private GameObject player; //ref to player
+
+    //script refs
+    private EntityHealth playerEntityHealth; //the entity health attached to the player object
+
+    //control vars
+    public bool debugToolsOn = false; //checks if debug tools are being used
+    public bool invincibilityOn = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ToggleDebug();
+        if (debugToolsOn)
+        {
+            ToggleDebugTools();
+        }
+	}
+
+    //Checks components and searches if null
+    void FindComponents()
+    {
+        //check if player ref exists
+        if (!player)
+        {
+            if (GameObject.FindGameObjectWithTag(playerTag))
+            {
+                player = GameObject.FindGameObjectWithTag(playerTag);
+            }
+        }
+
+        //if player ref exists, check for components of player that refs are needed
+        if (player)
+        {
+            if (!playerEntityHealth)
+            {
+                if (player.GetComponent<EntityHealth>())
+                {
+                    playerEntityHealth = player.GetComponent<EntityHealth>();
+                }
+            }
+        }
+    }
+
+    //Toggle for debug
+    private void ToggleDebug()
+    {
+        if (Input.GetKeyDown(debugToolToggleKey))
+        {
+            if (debugToolsOn)
+            {
+                debugToolsOn = false;
+            }
+            else
+            {
+                debugToolsOn = true;
+                //when turning on debug tools, check for components
+                FindComponents();
+            }
+        }
+    }
+
+    private void ToggleDebugTools()
+    {
+        if (Input.GetKeyDown(invincibilityToggleKey))
+        {
+            if (invincibilityOn)
+            {
+                invincibilityOn = false;
+                playerEntityHealth.enabled = true;
+            }
+            else
+            {
+                invincibilityOn = true;
+                playerEntityHealth.enabled = false;
+            }
+        }
+
+    }
+
+    //Toggle for invincibility
+    private void ToggleInvincibility()
+    {
+        if (Input.GetKeyDown(invincibilityToggleKey))
+        {
+            if (invincibilityOn)
+            {
+                invincibilityOn = false;
+                //activate players entity health script
+                playerEntityHealth.enabled = true;
+            }
+            else
+            {
+                invincibilityOn = true;
+                //deactivate players entity health script
+                playerEntityHealth.enabled = false;
+            }
+        }
+    }
+}
