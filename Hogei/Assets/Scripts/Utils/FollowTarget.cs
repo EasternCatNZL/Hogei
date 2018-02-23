ec18b5ebe79e866fa5b7c4df26659ba3cd28909b
Casey <chimpsterchip@gmail.com>
2018-02-23 16:12:24 +1300

    Folowtarget script + Adding Nav Obstacle
    Fence Small + Haystack

--- Hogei/Assets/Scripts/Utils/FollowTarget.cs
+++ Hogei/Assets/Scripts/Utils/FollowTarget.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class FollowTarget : MonoBehaviour {
 
+    public Transform Target;
+    public Transform CameraTransform;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        CameraTransform = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void LateUpdate () {
+        transform.position = Target.position;
 	}
 }
