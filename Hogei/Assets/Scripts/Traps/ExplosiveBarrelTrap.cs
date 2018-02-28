2a5b7ea4b13008082b83a4c22bc70e5f4f2f4b9c
Casey <chimpsterchip@gmail.com>
2018-02-28 18:26:19 +1300

    Implemented explosive traps

---
+++ Hogei/Assets/Scripts/Traps/ExplosiveBarrelTrap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveBarrelTrap : MonoBehaviour {
+
+    List<GameObject> ObjectsInRange;
+
+	// Use this for initialization
+	void Start () {
+        ObjectsInRange = new List<GameObject>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ObjectsInRange.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ObjectsInRange.Remove(other.gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        foreach (GameObject obj in ObjectsInRange)
+        {
+            if (obj.GetComponent<EntityHealth>())
+            {
+                obj.GetComponent<EntityHealth>().DecreaseHealth(1);
+            }
+        }
+    }
+}
