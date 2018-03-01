8083c08bd956287aa21c7a1d8093594ee72ccf6b
Casey <chimpsterchip@gmail.com>
2018-03-01 15:23:59 +1300

    Corn shake + added bullet shadow

---
+++ Hogei/Assets/Scripts/Enviroment/CornShake.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CornShake : MonoBehaviour {
+
+    public float ShakeLength = 1f;
+    public float ShakeIntensity = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        this.transform.DOShakeRotation(ShakeLength, ShakeIntensity);
+        //Add item drop
+    }
+}
