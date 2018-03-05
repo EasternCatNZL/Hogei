0c5f9fffd5f4aece90a05c10ac7966cff24bb862
Casey <chimpsterchip@gmail.com>
2018-03-05 15:57:51 +1300

    Made scripts to work on in the future

---
+++ Hogei/Assets/Scripts/Player/Attack/PlayerChainLighting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChainLighting : MonoBehaviour {
+
+    public float InitalRange = 10f;
+    public int NumberChains = 3;
+    public float ChainRange = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
