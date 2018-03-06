c3803f25b76566b8f70e938284fa3a1bcdc0007c
Karl Graves <karlgraves28@gmail.com>
2018-03-06 15:23:05 +1300

    fixed compiler arror in drop script

--- Hogei/Assets/Scripts/Enemy/Drops/HealthGlobe.cs
+++ Hogei/Assets/Scripts/Enemy/Drops/HealthGlobe.cs
@@ -7,13 +7,15 @@ public class HealthGlobe : MonoBehaviour {
 
     GameObject Player;
     public float HealthIncrease = 1.0f;
-    public float rotationSpeed = 1.0f * Time.deltaTime;
+    public float rotationSpeed;
 
 	// Use this for initialization
 	void Start () {
         Player = GameObject.FindGameObjectWithTag("Player");
         transform.DOJump(transform.position, 0.8f, 1, 0.5f);
-	}
+        rotationSpeed = 1.0f * Time.deltaTime;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
