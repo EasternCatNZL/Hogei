792dafe1f7a95200d35da35fadff10278f3516e2
Brian <brianpwwong@gmail.com>
2018-03-14 17:33:20 +1300

    Music menu start

---
+++ Hogei/Assets/Scripts/UI/Menus/MusicMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicMenu : MonoBehaviour {
+
+    [Header("Menu components")]
+    [Tooltip("Ref to master vol control")]
+    public Slider masterVolSlider;
+    [Tooltip("Ref to bgm slider")]
+    public Slider bgmVolSlider;
+    [Tooltip("Ref to sfx slider")]
+    public Slider sfxVolSlider;
+    
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
