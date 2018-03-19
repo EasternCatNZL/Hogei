adc9940435174bfbf6cc5b23f113e28d1e97ab1f
Karl Graves <karlgraves28@gmail.com>
2018-03-20 12:36:06 +1300

    Fert Gun in

--- Hogei/Assets/Scripts/BulletLogic/Player/CornExplosion.cs
+++ Hogei/Assets/Scripts/BulletLogic/Player/CornExplosion.cs
@@ -5,45 +5,55 @@ using DG.Tweening;
 
 public class CornExplosion : MonoBehaviour {
 
-    public float radius = 0.0f;
-    public float power = 0.0f;
-    public float upwardsForce = 0.0f;
+    //public float radius = 0.0f;
+    //public float power = 0.0f;
+    //public float upwardsForce = 0.0f;
 
     public float scaleSize = 0.0f;
     public float scaleDuration = 0.0f;
 
-    public float timer = 5.0f;
+    public float Damage = 1.0f;
+    public float timer = 0.3f;
+
+    public List<GameObject> ObjectsInRange;
+
+    //private bool hasExploded = false;
+    //private Vector3 ExplosionPos;
+
+     
         
 	// Use this for initialization
 	void Start () {
-		
+        //ExplosionPos = transform.position;
+        ObjectsInRange = new List<GameObject>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Scale();
         timer -= Time.deltaTime;
-        print(timer);
+        //print(timer);
         if(timer <= 0.0f)
         {
-            Explode();
-            Destroy(gameObject);
+            //Explode();            
+            OnDestroy();
         }         
 
 	}
 
     void Explode()
     {
-        
+        //hasExploded = true;
+        /*
         Vector3 explosionPos = transform.position;
         Collider[] Col = Physics.OverlapSphere(explosionPos, radius);
-        /*
+        
         foreach (Collider hit in Col)
         {
             GameObject temp = hit.gameObject;
             temp.GetComponent<EntityHealth>().DecreaseHealth(1);
         }
-        */
+        
         
         foreach (Collider hit in Col)
         {
@@ -56,11 +66,47 @@ public class CornExplosion : MonoBehaviour {
             }
             
         }
+        */
     }
 
     void Scale()
     {
         transform.DOScale(scaleSize, scaleDuration);
     }
-    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Enemy") || other.gameObject.tag.Equals("Player"))
+        {
+            ObjectsInRange.Add(other.gameObject);
+            print(other.gameObject.name);
+        }
+        /*
+        if (hasExploded == true && other.GetComponent<EntityHealth>())
+        {
+            //other.attachedRigidbody.AddExplosionForce(power, ExplosionPos, radius, upwardsForce);
+            other.gameObject.GetComponent<EntityHealth>().DecreaseHealth(1);
+            Destroy(gameObject);
+        }*/
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Enemy") || other.gameObject.tag.Equals("Player"))
+        {
+            ObjectsInRange.Remove(other.gameObject);
+        }
+    }
+    void OnDestroy()
+    {
+        foreach (GameObject obj in ObjectsInRange)
+        {
+            if (obj.GetComponent<EntityHealth>())
+            {
+                obj.GetComponent<EntityHealth>().DecreaseHealth(Damage);
+            }
+        }
+        Destroy(gameObject);
+    }
+
 }
