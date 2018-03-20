fe5acf5f448b3e976872c79bb6e159c7fdd134d2
Brian <brianpwwong@gmail.com>
2018-03-20 17:15:48 +1300

    Crab things

--- Hogei/Assets/Scripts/Enemy/Jellyfishing/HermitMoveBehavior.cs
+++ Hogei/Assets/Scripts/Enemy/Jellyfishing/HermitMoveBehavior.cs
@@ -7,10 +7,8 @@ public class HermitMoveBehavior : MonoBehaviour {
     [Header("Movement vars")]
     [Tooltip("Movement speed of the object")]
     public float moveSpeed = 5.0f;
-    [Tooltip("Travel point a")]
-    public Vector3 travelPointA = Vector3.zero;
-    [Tooltip("Travel point b")]
-    public Vector3 travelPointB = Vector3.zero;
+    [Tooltip("Travel points")]
+    public HermitCheckPointHandler[] travelPoints = new HermitCheckPointHandler[0];
 
     [Header("Timing vars")]
     [Tooltip("Time between attacks")]
@@ -19,18 +17,33 @@ public class HermitMoveBehavior : MonoBehaviour {
     //control vars
     private bool isActive = false; //check if object active
 
+    private int currentIndex = 0; //the current index of the array
+
     private Vector3 currentDestination = Vector3.zero;
     private Vector3 travelDirection = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
-		
+        SetupPointRefs();
+        //debug
+        currentDestination = travelPoints[0].transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
+        MoveBetweenPoints();
 	}
+
+    //Set up refs for all points
+    private void SetupPointRefs()
+    {
+        // for all points set up ref
+        for(int i = 0; i < travelPoints.Length; i++)
+        {
+            travelPoints[i].SetupPoint(this);
+        }
+    }
 
     //Move between points
     private void MoveBetweenPoints()
@@ -41,12 +54,20 @@ public class HermitMoveBehavior : MonoBehaviour {
             travelDirection = currentDestination - transform.position;
         }
         //move towards current destination
-        transform.position += transform.position + ((travelDirection * moveSpeed) * Time.deltaTime);
+        transform.position += (travelDirection * moveSpeed) * Time.deltaTime;
     }
 
     //change destination
-    public void ChangeDestination(Vector3 newDest)
+    public void ChangeDestination()
     {
-        currentDestination = newDest;
+        currentIndex++;
+        //if the index is equal to length of array, reset
+        if(currentIndex >= travelPoints.Length)
+        {
+            currentIndex = 0;
+        }
+        currentDestination = travelPoints[currentIndex].transform.position;
+        //change the travel direction
+        travelDirection = currentDestination - transform.position;
     }
 }
