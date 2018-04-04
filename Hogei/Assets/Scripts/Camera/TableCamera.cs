bfc7b689e65de2687643699c7d81401223725811
Casey <chimpsterchip@gmail.com>
2018-04-04 22:01:39 +1200

    Made an animation

---
+++ Hogei/Assets/Scripts/Camera/TableCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableMapCamera : MonoBehaviour {
+
+    [Header("Camera Settings")]
+    public float CameraSpeed;
+
+    [Header("Debug Settings")]
+    public Transform DebugSphere;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 NewCamPos = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+	}
+}
