e8d31b8f4436a924ca56061e112abad7b9259f01
Karl Graves <karlgraves28@gmail.com>
2018-04-04 14:41:44 +1200

    logic for infinite spawning enemy in place, started workign on spike trap

---
+++ Hogei/Assets/Scripts/Enemy/InfinateSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfinateSpawner : MonoBehaviour
+{
+
+    public GameObject Enemy;
+    public float SpawnTime = 1.0f;
+    private float timer = 0.0f;
+    private bool CanSpawn = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CanSpawn == true)
+        {
+            timer -= Time.deltaTime;            
+            if(timer <= 0.0f)
+            {
+                SpawnEnemy();                             
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            CanSpawn = true;
+            timer = SpawnTime;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            CanSpawn = false;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(Enemy, transform.position, transform.rotation);
+        timer = SpawnTime;
+    }
+}
