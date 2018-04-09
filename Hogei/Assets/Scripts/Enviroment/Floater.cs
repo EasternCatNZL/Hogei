a2115959e647a4aa53fbc5efacd343de61a9be76
Bear-PC\Bear <karlgraves28@gmail.com>
2018-04-09 23:09:33 +1200

    Spike trap logic in place

---
+++ Hogei/Assets/Scripts/Enviroment/Floater.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour {
+
+    //spin
+    public float DegreesPerSec = 0.0f;
+    //up/down
+    public float yAmp = 0.0f;
+    public float ySpeed = 0.0f;
+    //forward/backlwards
+    public float xAmp = 0.0f;
+    public float xSpeed = 0.0f;
+    //left/right
+    public float zAmp = 0.0f;
+    public float zSpeed = 0.0f;
+
+    Vector3 PositionOffset;
+    Vector3 TempPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        PositionOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0.0f, Time.deltaTime * DegreesPerSec, 0.0f), Space.World);
+        TempPos = PositionOffset;
+        TempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * ySpeed) * yAmp;
+        TempPos.x += Mathf.Sin(Time.fixedTime * Mathf.PI * xSpeed) * xAmp;
+        TempPos.z += Mathf.Sin(Time.fixedTime * Mathf.PI * zSpeed) * zAmp;
+        transform.position = TempPos;
+    }
+}
