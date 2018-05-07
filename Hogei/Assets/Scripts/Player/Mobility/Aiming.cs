bf34c34df7d9723644f6b19f3851670cde8e4020
Brian <brianpwwong@gmail.com>
2018-05-07 14:56:09 +1200

    Switch control schemes, controller support in <- I hope I didnt break shit

--- Hogei/Assets/Scripts/Player/Mobility/Aiming.cs
+++ Hogei/Assets/Scripts/Player/Mobility/Aiming.cs
@@ -19,6 +19,9 @@ public class Aiming : MonoBehaviour {
     [Header("Dead zone var")]
     public float deadZone = 0.5f;
 
+    [Header("Transform alignment")]
+    public Transform alignment;
+
     //control vars
     private bool controllerConnected = false;
 
@@ -67,7 +70,7 @@ public class Aiming : MonoBehaviour {
     private void ControllerInput()
     {
         //get direction from sticks
-        Vector3 direction = new Vector3(Input.GetAxis(rightStickX), 0.0f, Input.GetAxis(rightStickY));
+        Vector3 direction = new Vector3(Luminosity.IO.InputManager.GetAxis(rightStickX), 0.0f, Luminosity.IO.InputManager.GetAxis(rightStickY));
         //only work if meaningful
         if(direction.sqrMagnitude < deadZone)
         {
@@ -81,28 +84,28 @@ public class Aiming : MonoBehaviour {
             return;
         }
         //apply rotation
-        float angle = Mathf.Atan2(Input.GetAxis(rightStickX), Input.GetAxis(rightStickY)) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(Luminosity.IO.InputManager.GetAxis(rightStickX), Luminosity.IO.InputManager.GetAxis(rightStickY)) * Mathf.Rad2Deg;
         print(angle);
-        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
-        //set controller check timing
-        controllerCheck = controllerDelayCheck;
+        transform.rotation = Quaternion.Euler(0.0f, alignment.rotation.eulerAngles.y + angle, 0.0f);
+        ////set controller check timing
+        //controllerCheck = controllerDelayCheck;
     }
 
-    //check if mouse has moved enough to warrent change
-    private bool CheckMouseChange()
-    {
-        bool hasMoved = false;
-        //get the current pos
-        Vector3 currentPos = MouseTarget.GetWorldMousePos();
-        //check if it has moved enough to warrent input
-        if(currentPos.x > mouseLastPos.x + change || currentPos.x < mouseLastPos.x - change
-            || currentPos.y > mouseLastPos.y + change || currentPos.y < mouseLastPos.y - change
-            || currentPos.z > mouseLastPos.z + change || currentPos.z < mouseLastPos.z - change)
-        {
-            hasMoved = true;
-        }
+    ////check if mouse has moved enough to warrent change
+    //private bool CheckMouseChange()
+    //{
+    //    bool hasMoved = false;
+    //    //get the current pos
+    //    Vector3 currentPos = MouseTarget.GetWorldMousePos();
+    //    //check if it has moved enough to warrent input
+    //    if(currentPos.x > mouseLastPos.x + change || currentPos.x < mouseLastPos.x - change
+    //        || currentPos.y > mouseLastPos.y + change || currentPos.y < mouseLastPos.y - change
+    //        || currentPos.z > mouseLastPos.z + change || currentPos.z < mouseLastPos.z - change)
+    //    {
+    //        hasMoved = true;
+    //    }
 
 
-        return hasMoved;
-    }
+    //    return hasMoved;
+    //}
 }
