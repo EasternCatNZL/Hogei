5f193e90cccde957e24c67202958e528d2b274b7
Casey <chimpsterchip@gmail.com>
2018-05-23 17:33:42 +1200

    Adding some spicy to the game

---
+++ Hogei/Assets/CoopWalkerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Walking Chicken Coop
+/// Uses several different attacks to elimminate the play
+/// -Dive Attack: Lifts off into the air then crashes down into an aoe
+/// -Chicken Swarm: Shoots live chickens at the player
+/// -Slam Attack: Slams the ground and creates bullets
+/// </summary>
+public class CoopWalkerBehaviour : EnemyBehavior {
+
+
+    private GameObject Target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
