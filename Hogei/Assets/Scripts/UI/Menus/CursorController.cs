886a361ad37eb26696fcc197cc58bdd501782e82
EasternCatNZL <brianpwwong@gmail.com>
2018-06-08 17:35:25 +1200

    no message

--- Hogei/Assets/Scripts/UI/Menus/CursorController.cs
+++ Hogei/Assets/Scripts/UI/Menus/CursorController.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CursorController : MonoBehaviour {
 
-    [Header("Alignment transforms")]
-    public Transform alignment;
+    [Header("Cursor object")]
+    [Tooltip("Game representation of cursor")]
+    public GameObject cursorObject;
 
     [Header("Cursor move speed")]
     [Tooltip("Speed of cursor movement with controller")]
     public float cursorMoveSpeed = 5.0f;
+
+    [Header("Dead zone var")]
+    public float deadZone = 0.5f;
 
     [Header("Inputs")]
     public string contX = "CHorizontal";
@@ -23,7 +28,7 @@ public class CursorController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        canDo = GameObject.FindGameObjectWithTag(playerTag).GetComponent<WhatCanIDO>();
 	}
 
     void Awake()
@@ -33,27 +38,66 @@ public class CursorController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
         if (canDo.useKeyboard)
         {
             CursorFollow();
         }
         else if (canDo.useController)
         {
-
+            CursorMove();
         }
-	}
+    }
+
+    ////handle cursor distance from camera
+    //void CursorDistanceFromCamera()
+    //{
+    //    //get vector3 of set distance from alignments forward
+    //    Vector3 posAhead = alignment.forward * distanceFromCamera;
+    //    //set the 
+    //}
 
     //cursor should follow cursor when control set to keyboard
     void CursorFollow()
     {
-        //get the cursors pos in the world
-        Vector3 mousePos = MouseTarget.GetWorldMousePos();
-        //align self with camera
-        transform.rotation = alignment.rotation;
+        transform.position = Input.mousePosition;
     }
 
     //cursor is controlled  using the controller when on controller controls
     void CursorMove()
+    {
+
+        //vector 3 to direct movement
+        Vector3 moveDirection = Vector3.zero;
+
+        //get movement input from sticks
+        //x axis
+        if (Luminosity.IO.InputManager.GetAxisRaw(contX) > deadZone)
+        {
+            moveDirection += transform.right * cursorMoveSpeed * Luminosity.IO.InputManager.GetAxisRaw(contX);
+
+        }
+        else if (Luminosity.IO.InputManager.GetAxisRaw(contX) < -deadZone)
+        {
+            moveDirection += transform.right * cursorMoveSpeed * Luminosity.IO.InputManager.GetAxisRaw(contX);
+
+        }
+        //y axis
+        if (Luminosity.IO.InputManager.GetAxisRaw(contY) > deadZone)
+        {
+            moveDirection += transform.up * cursorMoveSpeed * Luminosity.IO.InputManager.GetAxisRaw(contY);
+
+        }
+        else if (Luminosity.IO.InputManager.GetAxisRaw(contY) < -deadZone)
+        {
+            moveDirection += transform.up * cursorMoveSpeed * Luminosity.IO.InputManager.GetAxisRaw(contY);
+        }
+        //move the object
+        transform.position = transform.position + moveDirection * cursorMoveSpeed * Time.deltaTime;
+    }
+
+    //when on controller controls, launch ray from cursor pos into world as if mouse behavior
+    private void CursorMouseBehavior()
     {
 
     }
